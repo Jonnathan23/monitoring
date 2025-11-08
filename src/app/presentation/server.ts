2e1629f2bf7229c162982b24c626f317076031c9
Jonnathan23 <jsaquicela26@gmail.com>
2025-11-07 21:27:22 -0500

    configuraciones para conectarse a BD mongo

--- src/app/presentation/server.ts
+++ src/app/presentation/server.ts
@@ -1,43 +1,8 @@
-import { CheckService } from "../domain/use-cases/checks/check-service.use";
-import { SendEmailLogs } from "../domain/use-cases/emails/send-email-logs.use";
-import { FileSystemDataSourceImpl } from "../infrastructure/datasources/file-system.datasource.impl";
-import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
-import { CronService } from "./service/cron/cron.service"
-import { EmailService } from "./service/email/email.service";
-
-const fileSystemLogRepository = new LogRepositoryImpl(
-    new FileSystemDataSourceImpl()
-);
-
-const emailService = new EmailService();
 
 export class Server {
 
     public static start() {
-        console.log('Server started');
-
-        // Mandar Email 
-        const sendEmailUseCase = new SendEmailLogs(emailService, fileSystemLogRepository);
-        sendEmailUseCase.execute(['jonnaferna@gmail.com', 'jonnasaquicela@gmail.com']);
-
-        // emailService.sendEmailWithFileSystemLogs(['jonnaferna@gmail.com', 'jonnasaquicela@gmail.com']);
-        // emailService.sendEmailWithFileSystemLogs(['jonnaferna@gmail.com', 'jonnasaquicela@gmail.com']);
-
-
-
-        //CronService.createJob(
-        //    "*/5 * * * * * ",
-        //    () => {
-        //        const checkService = new CheckService({
-        //            logRepository: fileSystemLogRepository,
-        //            succesCallback: undefined,//() => console.log(`success on check service`),
-        //            errorCallback: (error) => console.log(error)
-        //        })
-        //
-        //        checkService.execute('https://google.com');
-        //        //new CheckService().execute('http://localhost:3000');
-        //    });
-
+        console.log('Server started');     
 
     }
 }
